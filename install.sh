11d252928c6f67ff566e4bc12fc2a5edf8b18a2d
uncle_joe <uncle_joe@kali.kali.com>
2026-04-26 20:04:51 +0300

    make it readable

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir ~/.config/file-organizer/
-cp config.ini ~/.config/file-organizer
+cp config.ini ~/.config/file-organizer/
+chmod +r ~/.config/file-organizer/config.ini
 #optionally it makes sense to move build into /bin
